8d5450b7aa86857f9c0636bb020479829126e371
GiJung <101462387+GiJungPark@users.noreply.github.com>
2023-07-28 01:20:40 +0900

    [unrated] Title: 단어 퍼즐, Time: 77.54 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/1882. 단어 퍼즐/단어 퍼즐.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String[] strs, String t) {
+        int answer = 0;
+        int[] dp = new int[t.length()];
+        for(int i = t.length() - 1; i >= 0; i--){
+            for(String step : strs){
+                if(step.length() > t.length() - i) {
+                    continue;
+                } else if(step.equals(t.substring(i, i + step.length()))){
+                    if(t.length() - i == step.length()){
+                        dp[i] = 1;
+                        continue;
+                    }
+                    if(dp[i + step.length()] > 0){
+                    dp[i] = dp[i] == 0 ? dp[i + step.length()] + 1 : Math.min(dp[i], dp[i + step.length()] + 1);
+                    }   
+                }
+            }
+        }
+        answer = dp[0];
+        if(answer == 0) answer = -1;
+        return answer;
+    }
+}

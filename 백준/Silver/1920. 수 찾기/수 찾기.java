7f38b5668f371be6ff75540212342e2ef359b364
GiJung <101462387+GiJungPark@users.noreply.github.com>
2023-08-13 03:35:16 +0900

    [Silver IV] Title: 수 찾기, Time: 1620 ms, Memory: 69172 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,21 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int[] A = new int[N];
         for(int i = 0; i < N; i++){
-            A[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(A);
 
-        int M = sc.nextInt();
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for(int i = 0; i < M; i++){
             boolean flag = false;
-            int step = sc.nextInt();
+            int step = Integer.parseInt(st.nextToken());
             int start = 0;
             int end = N - 1;
             while(start <= end){

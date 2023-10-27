bd42019796d149f6b8f6c0d243507d40a6cb376a
GiJung <101462387+GiJungPark@users.noreply.github.com>
2023-10-27 22:38:26 +0900

    [Silver III] Title: 01타일, Time: 148 ms, Memory: 19288 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 2];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= N; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 15746;
+        }
+
+        bw.write(dp[N] + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}

ae895fee9918374a7f908f5c77da98d6566e8ca6
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-06-18 22:22:07 +0900

    [Silver V] Title: 줄세우기, Time: 488 ms, Memory: 26052 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static final String RESULT = "%d %d\n";
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int mainNumber = Integer.parseInt(st.nextToken());
+            List<Integer> list = new ArrayList<>();
+            list.add(Integer.parseInt(st.nextToken()));
+            int total = 0;
+
+            for (int j = 1; j < 20; j++) {
+                int currentNumber = Integer.parseInt(st.nextToken());
+
+                for(int k = 0; k < list.size(); k++) {
+                    if (list.get(k) > currentNumber) {
+                        total += list.size() - k;
+                        break;
+                    }
+                }
+
+                list.add(currentNumber);
+                Collections.sort(list);
+            }
+
+            bw.write(String.format(RESULT, mainNumber, total));
+            bw.flush();
+        }
+
+        bw.close();
+    }
+}

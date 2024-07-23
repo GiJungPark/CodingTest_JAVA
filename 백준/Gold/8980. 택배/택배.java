94ca769908b5225cc2ab0055369eb643920aeffd
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-07-23 14:43:26 +0900

    [Gold I] Title: 택배, Time: 356 ms, Memory: 21036 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.java
+++ 백준/Gold/8980. 택배/택배.java
@@ -1,76 +1,79 @@
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
+import static java.lang.Integer.parseInt;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 마을의 개수
-        int C = Integer.parseInt(st.nextToken()); // 최대 용량
-        int M = Integer.parseInt(br.readLine()); // 정보의 수
-        Tuple[] arr = new Tuple[M];
+        int N = parseInt(st.nextToken()); // 마을 수
+        int C = parseInt(st.nextToken()); // 저장 공간 수
+        int M = parseInt(br.readLine()); // 박스 수
 
-        for(int i = 0; i < M; i++){
+        List<Box> boxes = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cnt = Integer.parseInt(st.nextToken());
-            arr[i] = new Tuple(start, end, cnt);
+            int start = parseInt(st.nextToken());
+            int end = parseInt(st.nextToken());
+            int capacity = parseInt(st.nextToken());
+            boxes.add(new Box(start, end, capacity));
         }
 
-        Arrays.sort(arr);
-
-        int[] weights = new int[N + 1];
-        for(int i = 1; i <= N; i++)
-            weights[i] = C;
+        Collections.sort(boxes);
+        int[] arr = new int[N + 1];
 
         int result = 0;
-        for(int i = 0; i < M; i++){
-            Tuple tuple = arr[i];
-            int max = Integer.MAX_VALUE;
+        for (int i = 0; i < M; i++) {
+            Box box = boxes.get(i);
+            int start = box.start;
+            int end = box.end;
+            int capacity = box.capacity;
 
-            for(int j = tuple.start; j <= tuple.end; j++)
-                max = Math.min(max, weights[j]);
-
-            if(max >= tuple.capacity){
-                for(int j = tuple.start; j <= tuple.end; j++){
-                    weights[j] -= tuple.capacity;
+            int max = capacity;
+            for (int j = start; j < end; j++) {
+                if (arr[j] + capacity > C) {
+                    max = Math.min(max, C - arr[j]);
                 }
-                result += tuple.capacity;
-            }else{
-                for(int j = tuple.start; j <= tuple.end; j++){
-                    weights[j] -= max;
+//
+//                if (arr[j] + capacity <= C) {
+//                    arr[j] += capacity;
+//                }
+            }
+
+            if (max > 0) {
+                for (int j = start; j < end; j++) {
+                    arr[j] += max;
                 }
                 result += max;
             }
         }
-        
-        bw.write(result +"\n");
-        bw.flush();
-        bw.close();
-        br.close();
+
+        System.out.println(result);
     }
 
-    static class Tuple implements Comparable<Tuple>{
+    static class Box implements Comparable<Box> {
         int start, end, capacity;
-        Tuple(int start, int end, int capacity){
+
+        Box(int start, int end, int capacity) {
             this.start = start;
             this.end = end;
             this.capacity = capacity;
         }
 
         @Override
-        public int compareTo(Tuple o) {
-            if(this.end == o.end){
-                return this.start - o.end;
+        public int compareTo(Box o) {
+            if (this.end == o.end) {
+                return this.start - o.start;
             }
+
             return this.end - o.end;
         }
     }
